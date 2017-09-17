a5f4c133e1ec09c1579da5bc80e827acf2ea73c9
Ryan Van Etten <ryanve@gmail.com>
2017-09-16 17:17:56 -0700

    editFile(file, edit, cb)

---
+++ edit-file.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+const path = require("path")
+const utf8 = "utf8"
+const same = text => text
+const done = file => err => {
+  if (err) throw err
+  console.log("Edited:", file)
+}
+
+const editFile = (file, edit, cb) => {
+  if (void 0 === edit) edit = same
+  if (void 0 === cb) cb = done(file)
+  const abs = path.resolve(file)
+  fs.readFile(abs, utf8, (err, text) => {
+    if (err) throw err
+    text = edit(text)
+    fs.writeFile(abs, text, utf8, cb)
+  })
+}
+
+module.exports = editFile;
---
+++ test.js
@@ -0,0 +1,22 @@
+const editFile = require("./")
+const assert = require("assert")
+const fs = require("fs")
+const file = "test.txt"
+const utf8 = "utf8"
+const hello = "hello"
+const world = "world"
+
+fs.writeFile(file, hello, utf8, err => {
+  if (err) throw err
+  editFile(file, text => {
+    assert.strictEqual(text, hello)
+    return world
+  }, err => {
+    if (err) throw err
+    assert.strictEqual(fs.readFileSync(file, utf8), world)
+    fs.unlink(file, function(err) {
+      if (err) throw err
+      console.log("Tested:", file)
+    })
+  })
+})

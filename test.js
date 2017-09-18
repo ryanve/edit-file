de2a3dcb85cf16362bf17edadf388bf5a3daa65e
Ryan Van Etten <ryanve@gmail.com>
2017-09-17 23:39:23 -0700

    Test and correct noop error handling (#7)
    * Test that noop throws Error
    
    * Add else

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const editFile = require("./")
 const assert = require("assert")
 const fs = require("fs")
+const noop = () => {}
 const file = "test.txt"
 const utf8 = "utf8"
 const hello = "hello"
@@ -8,6 +9,7 @@ const world = "world"
 
 fs.writeFile(file, hello, utf8, err => {
   if (err) throw err
+  editFile(file, noop, err => assert.ok(err instanceof Error))
   editFile(file, text => {
     assert.strictEqual(text, hello)
     return world
